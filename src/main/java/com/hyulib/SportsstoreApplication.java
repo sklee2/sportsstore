ef7c05aded4cc642ee0585cea0340b6cf48e431d
user <user@HYU_LIB>
2017-03-04 17:41:10 +0900

    gradle change

--- src/main/java/com/hyulib/SportsstoreApplication.java
+++ src/main/java/com/hyulib/SportsstoreApplication.java
@@ -3,9 +3,12 @@ package com.hyulib;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class SportsstoreApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SportsstoreApplication.class, args);
 	}
