5c599a829bc249d97ad5151f50d8da0638a0e7d4
sklee <sklee2@me.com>
2017-04-16 17:41:57 +0900

    spring version up : 1.5.2

--- src/test/java/com/hyulib/SportsstoreApplicationTests.java
+++ src/test/java/com/hyulib/SportsstoreApplicationTests.java
@@ -3,14 +3,15 @@ package com.hyulib;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.hyulib.dao.ProductsDao;
 import com.hyulib.domain.Products;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SportsstoreApplication.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
 public class SportsstoreApplicationTests {
 	@Autowired
 	private ProductsDao productsDao;
